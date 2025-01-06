6b5af838b6c738768d3da145d79f9ddc15ecd276
karlach-a <karlach.argueta@gmail.com>
2025-01-05 20:21:28 -0600

    subiendo repositorio con todas las partes del proyecto que trabajamos entre Edwin, Hector, Karla

---
+++ actions/login_action.php
@@ -0,0 +1,34 @@
+<?php
+require '../includes/config.php';
+session_start();
+
+// Verificar si el usuario ha iniciado sesión
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = trim($_POST['username']);
+    $password = trim($_POST['password']);
+
+    if (empty($username) || empty($password)) {
+        die("Todos los campos son obligatorios.");
+    }
+
+    $stmt = $pdo->prepare("SELECT * FROM users WHERE username = ?");
+    $stmt->execute([$username]);
+    $user = $stmt->fetch();
+
+    if ($user && password_verify($password, $user['password'])) {
+        $_SESSION['user_id'] = $user['id'];
+        $_SESSION['role'] = $user['role'];
+        if ( $_SESSION['role'] !== 'admin') {
+            header('Location: ../views/admin_dashboard.php');
+            exit();
+        }
+        header('Location: ../views/user_account.php');
+        
+    } else {
+        header('Location: ../views/login.php?error=invalid_credentials');
+    }
+
+    
+    
+}
+?>
